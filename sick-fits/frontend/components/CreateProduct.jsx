b8a0100439f1fd8ba49576e374d3e1955fe07415
Michael Rojas <michaelrojasp@gmail.com>
2021-02-28 12:06:23 -0600

    Lesson 24, creating products via mutations

--- sick-fits/frontend/components/CreateProduct.jsx
+++ sick-fits/frontend/components/CreateProduct.jsx
@@ -1,23 +1,61 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
 import useForm from '../lib/useForm';
+import DisplayError from './ErrorMessage';
 import Form from './styles/Form';
 
-const CreateProduct = () => {
-  const { inputs, handleChange } = useForm({
-    name: '',
-    price: 0,
-    description: '',
-    image: '',
-  });
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    # Which variables are getting passed in? And What types are they
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
+export default function CreateProduct() {
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    image: '',
+    name: 'Nice Shoes',
+    price: 34234,
+    description: 'These are the best shoes!',
+  });
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
         console.log(inputs);
+        // Submit the inputfields to the backend:
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset aria-busy>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
+          Image
           <input
             required
             type="file"
@@ -27,6 +65,7 @@ const CreateProduct = () => {
           />
         </label>
         <label htmlFor="name">
+          Name
           <input
             type="text"
             id="name"
@@ -37,16 +76,18 @@ const CreateProduct = () => {
           />
         </label>
         <label htmlFor="price">
+          Price
           <input
             type="number"
             id="price"
             name="price"
-            placeholder="Price"
+            placeholder="price"
             value={inputs.price}
             onChange={handleChange}
           />
         </label>
         <label htmlFor="description">
+          Description
           <textarea
             id="description"
             name="description"
@@ -55,10 +96,9 @@ const CreateProduct = () => {
             onChange={handleChange}
           />
         </label>
-        <button type="submit">Add Product</button>
+
+        <button type="submit">+ Add Product</button>
       </fieldset>
     </Form>
   );
-};
-
-export default CreateProduct;
+}
