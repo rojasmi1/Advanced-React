d9aabc7aa0c6cc5e132c5858296d35df295603a7
Michael Rojas <michaelrojasp@gmail.com>
2021-02-28 21:55:57 -0600

    Lesson 25&26: Re-fetch products

--- sick-fits/frontend/components/CreateProduct.jsx
+++ sick-fits/frontend/components/CreateProduct.jsx
@@ -1,7 +1,9 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
+import Router from 'next/router';
 import useForm from '../lib/useForm';
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from './Products';
 import Form from './styles/Form';
 
 const CREATE_PRODUCT_MUTATION = gql`
@@ -40,16 +42,16 @@ export default function CreateProduct() {
     CREATE_PRODUCT_MUTATION,
     {
       variables: inputs,
+      refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     }
   );
   return (
     <Form
       onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
-        // Submit the inputfields to the backend:
-        await createProduct();
+        const response = await createProduct();
         clearForm();
+        Router.push({ pathname: `/product/${response.data.createProduct.id}` });
       }}
     >
       <DisplayError error={error} />
