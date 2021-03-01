dba5121c030e25d6d341e0ec6f7e462bf9d5beab
Michael Rojas P <michael.rojas@gorillalogic.com>
2021-03-01 09:16:50 -0600

    Initial commit to migrate from personal repo to company's repo

---
+++ sick-fits/frontend/pages/index.jsx
@@ -0,0 +1,9 @@
+// export default function IndexPage() {
+//   return (
+//     <div>
+//       <p>Hello!</p>
+//     </div>
+//   );
+// }
+
+export { default } from './products';
---
+++ sick-fits/frontend/pages/sell.jsx
@@ -0,0 +1,10 @@
+import CreateProduct from '../components/CreateProduct';
+
+export default function SellPage() {
+  return (
+    <div>
+      <p>Hello!</p>
+      <CreateProduct />
+    </div>
+  );
+}
